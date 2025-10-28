33a433853bf66178f189fe4155ebee4a2da9f2d8
leyder-marcillo <39281890+leyder-marcillo@users.noreply.github.com>
2025-10-28 10:30:11 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,10 @@ export default defineConfig({
     process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
           await import("@replit/vite-plugin-dev-banner").then((m) =>
-            m.devBanner(),
+            m.devBanner()
           ),
         ]
       : []),
@@ -29,11 +29,5 @@ export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: true,
-  },
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
-    },
   },
 });
